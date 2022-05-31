34ac2b1f501ee4a28c2d33af1bdcb5802de52420
Daniel Marin Sereno <86926807+DanielSerenoPD@users.noreply.github.com>
2022-05-30 23:33:08 -0500

    Update EmployeesController.js

--- controllers/EmployeesController.js
+++ controllers/EmployeesController.js
@@ -63,3 +63,75 @@ export const deleteEmployee = async (req, res) => {
         res.json({ message: error.message })
     }
 }
+export const saveDirectory = async(){
+  try{
+      if(req.file){
+      const excel = xlsx.readFile(req.file);
+      const sheetNames = excel.SheetNames;
+      const totalSheets = sheetNames.length;
+      let parsedData = [];
+  for (let i = 0; i < totalSheets; i++) {
+      const tempData = xlsx.utils.sheet_to_json(excel.Sheets[sheetNames[i]]);
+      tempData.shift();
+      parsedData.push(...tempData);
+  }
+  var employeeQuery = new Parse.Query(Employee)
+  let i = 0;
+  for(let item of parsedData){
+  //let item = parsedData[i];
+  employeeQuery.equalTo('nomina',+item.nomina);
+  let result = await employeeQuery.find();
+  let isSave = null
+  if(result.length>0){
+      let employeeUpdate = result[0];
+      employeeUpdate.save()
+      .then(employee=>{
+          employee.set("nominaId",""+item.nomina);
+          employee.set("nomina",+item.nomina);
+          employee.set("name",item.nombre);
+          employee.set("job",item.puesto);
+            employee.set("location",item.ubicacion);
+            employee.set("companyEmail",item.email);
+            employee.set("companyPhone",""+item.telefono)
+            employee.set("personalPhone",""+item.telefonopersonal);
+            employee.set("extension",""+item.extension);
+            employee.set("status",item.status === "ACTIVE"?true:false);
+            employee.set("retired",item.jubilado == "x"?true:false);
+            employee.set("personalEmail",item.emailpersonal);
+            employee.set("imageUrl",item.foto);
+            return employee.save();
+        })
+        console.log("existe"+i)
+    }else{
+        let employee = new Employee();
+        isSave = await employee.save({
+        nomina: +item.nomina,
+        nominaId: ""+item.nomina,
+        name: item.nombre,
+        job: item.puesto,
+        location: item.ubicacion,
+        companyEmail: item.email,
+        companyPhone: ""+item.telefono,
+        extension: ""+item.extension,
+        status: item.status === "ACTIVE"?true:false,
+        retired: item.jubilado == "x"?true:false,
+        personalEmail: item.emailpersonal,
+        personalPhone: ""+item.telefonopersonal,
+        imageUrl: item.foto
+      });
+      console.log("no existe" + i)
+    }
+    i++;
+  }
+    
+  console.log("Directorio actualizado correctamente.")
+  }else{
+      console.log("El directorio ya estaba actualizado.")
+  }
+  
+  return true;
+  }catch(error){
+      console.log("Ha ocurrido un error actualizando el directorio.")
+      return false;
+  }
+}
