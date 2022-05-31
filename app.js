663a4dbdb8228ced6f7aa514e261ac7fe5a3f835
Daniel Marin Sereno <86926807+DanielSerenoPD@users.noreply.github.com>
2022-05-30 23:30:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ import express from "express";
 import cors from 'cors';
 import db from "./database/db.js";
 import employeeRoutes from "./routes/routes.js"
-
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    //let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+    console.log(file)
+    cb(null, file.originalname)
+  }
+})
+ 
+var upload = multer({ storage: storage })
 const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use('/employees', employeeRoutes)
+app.use('/employees',upload.single('file'), employeeRoutes)
 
 try {
     await db.authenticate()
