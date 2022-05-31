b0a63f5a0d717d6c141b430ceedd615cd1b1f8ce
Daniel Marin Sereno <86926807+DanielSerenoPD@users.noreply.github.com>
2022-05-31 10:02:37 -0500

    Update EmployeesController.js

--- controllers/EmployeesController.js
+++ controllers/EmployeesController.js
@@ -74,75 +74,56 @@ export const deleteEmployee = async (req, res) => {
         res.json({ message: error.message })
     }
 }
-export const saveDirectory = async() => {
-  try{
-      if(req.file){
-      const excel = xlsx.readFile(req.file);
-      const sheetNames = excel.SheetNames;
-      const totalSheets = sheetNames.length;
-      let parsedData = [];
-  for (let i = 0; i < totalSheets; i++) {
-      const tempData = xlsx.utils.sheet_to_json(excel.Sheets[sheetNames[i]]);
-      tempData.shift();
-      parsedData.push(...tempData);
-  }
-  var employeeQuery = new Parse.Query(Employee)
-  let i = 0;
-  for(let item of parsedData){
-  //let item = parsedData[i];
-  employeeQuery.equalTo('nomina',+item.nomina);
-  let result = await employeeQuery.find();
-  let isSave = null
-  if(result.length>0){
-      let employeeUpdate = result[0];
-      employeeUpdate.save()
-      .then(employee=>{
-          employee.set("nominaId",""+item.nomina);
-          employee.set("nomina",+item.nomina);
-          employee.set("name",item.nombre);
-          employee.set("job",item.puesto);
-            employee.set("location",item.ubicacion);
-            employee.set("companyEmail",item.email);
-            employee.set("companyPhone",""+item.telefono)
-            employee.set("personalPhone",""+item.telefonopersonal);
-            employee.set("extension",""+item.extension);
-            employee.set("status",item.status === "ACTIVE"?true:false);
-            employee.set("retired",item.jubilado == "x"?true:false);
-            employee.set("personalEmail",item.emailpersonal);
-            employee.set("imageUrl",item.foto);
-            return employee.save();
-        })
-        console.log("existe"+i)
-    }else{
-        let employee = new Employee();
-        isSave = await employee.save({
-        nomina: +item.nomina,
-        nominaId: ""+item.nomina,
-        name: item.nombre,
-        job: item.puesto,
-        location: item.ubicacion,
-        companyEmail: item.email,
-        companyPhone: ""+item.telefono,
-        extension: ""+item.extension,
-        status: item.status === "ACTIVE"?true:false,
-        retired: item.jubilado == "x"?true:false,
-        personalEmail: item.emailpersonal,
-        personalPhone: ""+item.telefonopersonal,
-        imageUrl: item.foto
-      });
-      console.log("no existe" + i)
+export const importEmployees = async () =>{
+        const fileName = "example-employees.csv";
+
+        csv().fromFile(fileName).then(source => {
+            for (var i = 0; i < source.length; i++) {
+                var nomina = source[i]["nomina"],
+                    nombre = source[i]["nombre"],
+                    puesto = source[i]["puesto"],
+                    ubicacion = source[i]["ubicacion"],
+                    correo = source[i]["correo"],
+                    telefono = source[i]["telefono"],
+                    extension = source[i]["extension"],
+                    status = source[i]["status"],
+                    jubilado = source[i]["jubilado"],
+                    correop = source[i]["correop"],
+                    telefonop = source[i]["telefonop"],
+                    foto = source[i]["foto"]
+                    
+                    if(nomina.length===0){nomina="!"}
+                    if(nombre.length===0){ nombre="!"}
+                    if(puesto.length===0){puesto="!"}
+                    if(ubicacion.length===0){ubicacion="!"}
+                    if(correo.length===0){ correo="!"}
+                    if(telefono.length===0){ telefono="!" }
+                    if(extension.length===0){ extension="!" }
+                    if(status.length===0){ status="!" }
+                    if(jubilado.length===0){ jubilado="no" }
+                    if(telefonop.length===0){ telefonop="!" }
+                    if(correop.length===0){ correop="!"}
+                    if(foto.length===0){ foto="!" }
+
+                    if(nomina==="!" && nombre==="!" && puesto==="!" && ubicacion==="!" && correo==="!" && telefono==="!" && extension==="!" && status==="!" && correop==="!" && telefonop==="!" && foto==="!"){
+                        console.log("Campos vaciosssss")
+                    }else{
+                        if (jubilado==="X") {
+                            jubilado = "si"
+                        }
+                        var insertStatement = `INSERT INTO empleados values(null,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+                        var items = [nomina, nombre, puesto, ubicacion, correo, telefono, extension, status, jubilado, correop, telefonop, foto];
+                        
+                        try {
+        await EmployeesModel.create(insertStatement,items)
+        console.log("insertado")
+    } catch (error) {
+        console.log("error")
     }
-    i++;
-  }
-    
-  console.log("Directorio actualizado correctamente.")
-  }else{
-      console.log("El directorio ya estaba actualizado.")
-  }
-  
-  return true;
-  }catch(error){
-      console.log("Ha ocurrido un error actualizando el directorio.")
-      return false;
-  }
-}
+                    }
+            }  
+        });
+     res.json({
+            "message":"Registro completo"
+        })
+    } 
