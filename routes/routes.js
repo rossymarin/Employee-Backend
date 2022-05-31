57e868c44eceba744a6d873f4ca25dd2a7afd3e0
Daniel Marin Sereno <86926807+DanielSerenoPD@users.noreply.github.com>
2022-05-30 23:35:55 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAll, getEmployee, createEmployee, updateEmployee, deleteEmployee } from '../controllers/EmployeesController.js';
+import { getAll, getEmployee, createEmployee, updateEmployee, deleteEmployee, saveDirectory} from '../controllers/EmployeesController.js';
 const router = express.Router()
 
 router.get('/', getAll)
 router.get('/:id', getEmployee)
 router.post('/', createEmployee)
+router.post('/upload', saveDirectory)
 router.put('/:id', updateEmployee)
 router.delete('/:id', deleteEmployee)
 
